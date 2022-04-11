1947ba7553a912de6c44eaa3ef906d05791bac17
JJ <juanfragamadrid@gmail.com>
2022-04-11 12:54:18 +0200

    Varios/aaa 4

--- src/movimientos/Aaaa.java
+++ src/movimientos/Aaaa.java
@@ -13,4 +13,40 @@ public class Aaaa
 {
     private String _nombre_2 = "goooooooooooog";
     private String _nombre_3 = "goooooooooooog";
+    private int _estatura = 0;
+
+    public String getNombre_2()
+    {
+        return _nombre_2;
+    }
+
+    public void setNombre_2(String _nombre_2)
+    {
+        this._nombre_2 = _nombre_2;
+    }
+
+    public String getNombre_3()
+    {
+        return _nombre_3;
+    }
+
+    public void setNombre_3(String _nombre_3)
+    {
+        this._nombre_3 = _nombre_3;
+    }
+
+    public int getEstatura()
+    {
+        return _estatura;
+    }
+
+    public void setEstatura(int _estatura)
+    {
+        this._estatura = _estatura;
+    }
+
+    public Aaaa(String nombre)
+    {
+        this._nombre_2 = nombre;
+    }
 }
