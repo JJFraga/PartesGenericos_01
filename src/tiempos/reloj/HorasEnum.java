3230a6eac61f21ab1a8486664ea0a53132e9c125
JJ <juanfragamadrid@gmail.com>
2022-04-11 16:37:51 +0200

    Reloj

--- src/tiempos/reloj/HorasEnum.java
+++ src/tiempos/reloj/HorasEnum.java
@@ -3,16 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package configuracion.emisores;
+package tiempos.reloj;
 
 /**
  *
  * @author JJ
  */
-public class Emisor
+public enum HorasEnum
 {
-    public Emisor()
-    {
 
-    }
 }
